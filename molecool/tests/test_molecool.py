443acb84771941eb569b72e6fe1cba497e37459c
Sevy Harris <harris.se@northeastern.edu>
2021-01-13 09:38:59 -0500

    added code for homework assignment

--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -23,6 +23,16 @@ def methane_molecule():
     return symbols, coordinates
 
 
+@pytest.mark.parametrize("p1, p2, p3, expected_value", [
+    (np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]),np.array([0, 0, 0]), np.array([1, 0, 0]), 45),
+    (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60),
+    (np.array([np.sqrt(3)/2, (1/2), 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 30),
+])
+def test_calculate_angle_many(p1, p2, p3, expected_value):
+    calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
+    assert expected_value == pytest.approx(calculated_angle), f'{calculated_angle} {expected_value}'
+
+
 def test_calculate_distance():
     """Sample test to check the distance calculation"""
     r1 = np.array([0, 0, 0])
@@ -32,7 +42,6 @@ def test_calculate_distance():
     assert (expected_behavior == observed_behavior)
 
 
-@pytest.mark.skip
 def test_build_bond_list_failure(methane_molecule):
     """Sample test to check the bond list"""
     _, coordinates = methane_molecule
@@ -47,6 +56,25 @@ def test_build_bond_list(methane_molecule):
     assert (len(bonds) == 4)
     for bond_length in bonds.values():
         assert bond_length == 1.4
+
+
+def test_calculate_angle():
+    r1 = np.array([0, 0, -1])
+    r2 = np.array([0, 0, 0])
+    r3 = np.array([1, 0, 0])
+    expected_value = 90
+    calculated_value = molecool.calculate_angle(r1, r2, r3, degrees=True)
+    assert expected_value == calculated_value
+
+
+def test_molecular_mass():
+    symbols = ['C', 'H', 'H', 'H', 'H']
+
+    calculated_mass = molecool.calculate_molecular_mass(symbols)
+
+    actual_mass = 16.04
+
+    assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
 
 
 def test_molecool_imported():
